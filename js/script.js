12deb18719b763699298142152ea5d5d52e0d136
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-10 12:43:53 +0300

    set up sliders && set breakpoints

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ import Swiper from "../node_modules/swiper/swiper-bundle.mjs"
 const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
-    loop: true
+    loop: true,
+    slidesPerView: 1,
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        1110: {
+            slidesPerView: 3,
+        },
+    }
 })
 
