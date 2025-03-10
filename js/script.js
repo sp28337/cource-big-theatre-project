0389ef53cde8ab53befbf6b21e42fa4d4626edef
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-10 12:28:06 +0300

    create swiper

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi")
+
+import Swiper from "../node_modules/swiper/swiper-bundle.mjs"
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true
+})
+
